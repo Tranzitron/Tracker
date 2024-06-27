db26fa772249ed0475d5e8c6f4866f5e2731b640
Raphael Vallée <72668371+Tranzitron@users.noreply.github.com>
2024-06-27 14:45:31 -0400

    Auth - Profile Page

--- tracker/lib/auth_page.dart
+++ tracker/lib/auth_page.dart
@@ -1,31 +1,10 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'home_page.dart';
 
-class AuthPage extends StatefulWidget {
+class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
-
-  @override
-  State<AuthPage> createState() => _AuthPageState();
-}
-
-class _AuthPageState extends State<AuthPage> {
-  @override
-  void initState() {
-    super.initState();
-
-    FirebaseAuth.instance.authStateChanges().listen((User? user) async {
-      if (user != null) {
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-            builder: (context) => const HomePage(),
-          ),
-        );
-      }
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +13,21 @@ class _AuthPageState extends State<AuthPage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Auth'),
       ),
-      body: const SignInScreen(),
+      body: SignInScreen(
+        actions: [
+          AuthStateChangeAction(
+            (context, authState) {
+              if (authState.runtimeType == SignedIn) {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => const HomePage(),
+                  ),
+                );
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 }
