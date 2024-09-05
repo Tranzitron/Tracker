72a42c5f0730ad21233f12f14809583b6732bfec
Raphael <72668371+Tranzitron@users.noreply.github.com>
2024-09-05 15:43:50 -0400

    First Isar Implementation

--- tracker/lib/home_page.dart
+++ tracker/lib/home_page.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
+import 'package:isar/isar.dart';
+import 'package:tracker/main.dart';
+import 'package:tracker/user.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,26 +13,82 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late Isar isar;
+  late List<User> users = [];
+
+  @override
+  void initState() {
+    super.initState();
+    initDb();
+  }
+
+  Future<void> initDb() async {
+    isar = await DbInstance.getIsar();
+    refreshUsers();
+  }
+
+  void refreshUsers() async {
+    users = await isar.users.where().findAll();
+    setState(() {
+      users;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(
-        middle: Text('Home'),
+      navigationBar: CupertinoNavigationBar(
+        middle: const Text('Home'),
+        trailing: CupertinoButton(
+          padding: const EdgeInsets.all(0),
+          child: const Text("Add"),
+          onPressed: () async {
+            final newUser = User()
+              ..name = 'Jane Doe'
+              ..age = Random().nextInt(100);
+
+            await isar.writeTxn(() async {
+              await isar.users.put(newUser); // insert & update
+            });
+
+            refreshUsers();
+          },
+        ),
+        leading: CupertinoButton(
+          padding: const EdgeInsets.all(0),
+          child: const Text("Clear"),
+          onPressed: () async {
+            await isar.writeTxn(
+              () async {
+                //await isar.users.where().deleteAll();
+                await isar.users.clear();
+              },
+            );
+            refreshUsers();
+          },
+        ),
       ),
       resizeToAvoidBottomInset: true,
       child: SafeArea(
         child: Center(
-          child: CupertinoListSection(
-            children: List.generate(
-              10,
-              (index) {
-                return CupertinoListTile(
-                  title: Text("Hi $index"),
-                  trailing: const CupertinoListTileChevron(),
-                );
-              },
-            ),
-          ),
+          child: users.isNotEmpty
+              // fix list too long overflow
+              ? CupertinoListSection(
+                  children: List.generate(
+                    users.length,
+                    (index) {
+                      final user = users[index];
+                      return CupertinoListTile(
+                        title: Text("Hi ${user.name} : ${user.id}"),
+                        trailing: const CupertinoListTileChevron(),
+                      );
+                    },
+                  ),
+                )
+              : const SizedBox(
+                  width: 0,
+                  height: 0,
+                ),
         ),
       ),
     );
