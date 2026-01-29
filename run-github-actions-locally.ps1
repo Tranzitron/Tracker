ccfb28edea6554f0490f307176f964fb9dc94656
Raphael Vallée <72668371+Tranzitron@users.noreply.github.com>
2026-01-28 23:21:45 -0500

    ps1:Install act-cli, internet check, docker-desktop

--- run-github-actions-locally.ps1
+++ run-github-actions-locally.ps1
@@ -1,33 +1,61 @@
-$currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
-$isAdmin = (New-Object Security.Principal.WindowsPrincipal $currentUser).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+function RunAsAdmin {
+	$currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
+	$isAdmin = (New-Object Security.Principal.WindowsPrincipal $currentUser).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
-if (-not $isAdmin) {
-	Start-Process powershell.exe -ArgumentList "-File `"$PSCommandPath`"" -Verb RunAs
-	exit
+	if (-not $isAdmin) {
+		Start-Process powershell.exe -ArgumentList "-File `"$PSCommandPath`"" -Verb RunAs
+		exit
+	}
 }
+
+RunAsAdmin
 
 function Test-CommandExists ($cmdname) {
 	return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
 }
 
-$commandName = "act"
-if (-Not (Test-CommandExists -cmdname $commandName)) {
-	Write-Host "$commandName isn't installed."
-	Write-Host "installing act"
-	
-	if (Test-CommandExists -cmdname 'winget') {
-		#winget install nektos.act 
-		winget install -e --id nektos.act --disable-interactivity --silent --accept-package-agreements --accept-source-agreements
-	}
- else {
-		choco upgrade act-cli -y
-	}
+if (-not (Test-NetConnection -ComputerName www.google.com -InformationLevel Quiet)) {
+	Write-Host "An Internet connection is required." -ForegroundColor Red
+	Pause
+	exit
 }
 
-if (-Not (Test-CommandExists -cmdname $commandName)) {
-	Write-Host Couldnt install "act"
-	pause
-	exit
+$commandName = "act"
+if (-not (Test-CommandExists -cmdname $commandName)) {
+	$actInstalled = $false
+	Write-Host "$commandName isn't installed."
+	Write-Host "Trying to install act.."
+	
+	if (Test-CommandExists -cmdname 'winget') {
+		winget install -e --id nektos.act --disable-interactivity --silent --accept-package-agreements --accept-source-agreements
+		if (Test-CommandExists -cmdname $commandName) {
+			$actInstalled = $true
+		}
+	}
+
+	if (-not $actInstalled) {
+		if (Test-CommandExists -cmdname 'choco') {
+			choco upgrade act-cli -y
+			if (Test-CommandExists -cmdname $commandName) {
+				$actInstalled = $true
+			}
+		}
+		else {
+			Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+			if (Test-CommandExists -cmdname 'choco') {
+				choco upgrade act-cli -y
+				if (Test-CommandExists -cmdname $commandName) {
+					$actInstalled = $true
+				}
+			}
+		}
+	}
+
+	if (-not (Test-CommandExists -cmdname $commandName)) {
+		Write-Host Couldnt install "act"
+		Pause
+		exit
+	}
 }
 
 $serviceName = "com.docker.service"
@@ -48,8 +76,18 @@ elseif ($service) {
 }
 else {
 	Write-Host "DOCKER ISNT INSTALLED"
+	try {
+		# Required for both WSL and Docker
+		Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -All
+	}
+	catch {
+		Write-Host "Couln't Enable Windows Feature: 'VirtualMachinePlatform'" -ForegroundColor Red
+		Write-Host $_
+		Pause
+		exit
+	}
+	wsl --install --no-distribution
+	choco upgrade -y docker-desktop
 }
 
-
-
-pause
+Pause
